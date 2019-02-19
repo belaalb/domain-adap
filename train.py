d719a66f731e01a29d57dce10925735186ffe795
Isabela <isabelamcalbuquerque@gmail.com>
2019-02-19 14:51:19 -0500

    first commit

---
+++ train.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import random
+
+import torch.optim as optim
+import torch.utils.data
+from torchvision import datasets
+from torchvision import transforms
+
+import models as models_
+from train_loop import TrainLoop
+from data_loader import Loader
+
+parser = argparse.ArgumentParser(description='Reproducing DANN')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 64)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N', help='number of epochs to train (default: 50)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR', help='learning rate (default: 0.0002)')
+parser.add_argument('--checkpoint-epoch', type=int, default=None, metavar='N', help='epoch to load for checkpointing. If None, training starts from scratch')
+parser.add_argument('--checkpoint-path', type=str, default=None, metavar='Path', help='Path for checkpointing')
+parser.add_argument('--source', choices=['mnist', 'mnist_m'], default='mnist', help='Path to source data')
+parser.add_argument('--target', choices=['mnist', 'mnist_m'], default='mnist_m', help='Path to target data')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+parser.add_argument('--save-every', type=int, default=5, metavar='N', help='how many epochs to wait before logging training status. Default is 5')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
+
+args = parser.parse_args()
+args.cuda = True if not args.no_cuda and torch.cuda.is_available() else False
+
+# Setting seed
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+	torch.cuda.manual_seed(args.seed)
+
+source_path = os.path.join('.', 'data', args.source)
+target_path = os.path.join('.', 'data', args.target)
+
+image_size = 28
+img_transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+source_dataset = datasets.MNIST(root=source_path, train=True, transform=img_transform)
+source_loader = torch.utils.data.DataLoader(dataset=source_dataset, batch_size=batch_size, shuffle=True, num_workers=args.workers)
+
+train_list = os.path.join(target_image_root, 'mnist_m_train_labels.txt')
+target_dataset = Loader(data_root=os.path.join(target_path, 'mnist_m_train'), data_list=train_list, transform=img_transform)
+target_loader = torch.utils.data.DataLoader(dataset=target_dataset, batch_size=batch_size, shuffle=True, num_workers=args.workers)
+
+model = models_.CNNModel()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+if args.cuda:
+	model = model.cuda()
+	torch.backends.cudnn.benchmark=True
+
+trainer = TrainLoop(model, optimizer, source_loader, target_loader, checkpoint_path=args.checkpoint_path, checkpoint_epoch=args.checkpoint_epoch, cuda=args.cuda)
+
+print('Cuda Mode: {}'.format(args.cuda))
+print('Batch size: {}'.format(args.batch_size))
+print('LR: {}'.format(args.lr))
+print('Source: {}'.format(args.source))
+print('Target: {}'.format(args.target))
+
+trainer.train(n_epochs=args.epochs, save_every=args.save_every)
+
+
---
+++ train_loop.py
@@ -0,0 +1,137 @@
+import os
+import pickle
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from scipy.optimize import minimize
+from torch.autograd import Variable
+from tqdm import tqdm
+
+
+class TrainLoop(object):
+
+	def __init__(self, model, optimizer, source_loader, target_loader, checkpoint_path=None, checkpoint_epoch=None, cuda=True):
+		if checkpoint_path is None:
+			# Save to current directory
+			self.checkpoint_path = os.getcwd()
+		else:
+			self.checkpoint_path = checkpoint_path
+			if not os.path.isdir(self.checkpoint_path):
+				os.mkdir(self.checkpoint_path)
+
+		self.save_epoch = os.path.join(self.checkpoint_path, 'cp_{}ep.pt')
+
+		self.cuda_mode = cuda
+		self.model = model
+		self.optimizer = optimizer
+		self.source_loader = source_loader
+		self.target_loader = target_loader
+		self.history = {'loss': []}
+		self.total_iters = 0
+		self.cur_epoch = 0
+
+	def train(self, n_epochs=1, save_every=1):
+
+		len_dataloader = min(len(source_loader), len(target_loader))
+		source_iter = tqdm(iter(source_loader))
+		target_iter = iter(target_loader)
+
+		while self.cur_epoch < n_epochs:
+
+			print('Epoch {}/{}'.format(self.cur_epoch + 1, n_epochs))
+
+			i = 0
+			cur_loss
+			while i < len_dataloader:
+				p = float(i + self.cur_epoch * len_dataloader) / n_epochs / len_dataloader
+				self.alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+				batch_source = source_iter.next()
+				batch_target = target_iter.next()
+				cur_loss += self.train_step(batch_source, batch_target)
+				i += 1
+			
+			self.cur_epoch += 1
+
+			if self.cur_epoch % save_every == 0:
+				self.checkpointing()
+
+			self.history['loss'].append(cur_loss/i)
+
+			print('Current loss: {}.'.format(cur_loss/i))
+
+		# saving final models
+		print('Saving final model...')
+		self.checkpointing()
+
+
+	def train_step(self, batch_source, batch_target):
+		self.model.train()
+
+		x_source, y_source = batch_source
+		source_labels = torch.zeros(y_source.size())
+
+		#TODO: 3 channels 
+		input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+
+		x_target, _ = batch_target
+		target_labels = torch.zeros(y_target.size())
+
+		if self.cuda_mode:
+			x_source = x_source.cuda()
+			y_source = y_source.cuda()
+			source_labels = source_labels.cuda() 
+
+		class_out, domain_out = self.model.forward(x_source, self.alpha)		
+		loss_class = torch.nn.NLLLoss()(class_out, y_source)
+		loss_source = torch.nn.NLLLoss()(domain_out, source_labels)
+		
+		if self.cuda_mode:
+			x_target = x_target.cuda()
+			target_labels = target_labels.cuda()
+
+		_, domain_out = self.model.forward(x_target, self.alpha)		
+		loss_target = torch.nn.NLLLoss()(domain_out, y_target)
+
+		loss = loss_class + loss_source + loss_target 
+
+		loss.backward()
+		self.optimizer.step()
+
+		return loss.item() 
+
+
+	def checkpointing(self):
+		# Checkpointing
+		print('Checkpointing...')
+		ckpt = {'model_state': self.model.state_dict(),
+				'optimizer_state': self.optimizer.state_dict(),
+				'scheduler_state': self.scheduler.state_dict(),
+				'history': self.history,
+				'total_iters': self.total_iters,
+				'cur_epoch': self.cur_epoch}
+		torch.save(ckpt, self.save_epoch.format(self.cur_epoch))
+
+	def load_checkpoint(self, epoch):
+		ckpt = self.save_epoch.format(epoch)
+
+		if os.path.isfile(ckpt):
+
+			ckpt = torch.load(ckpt)
+			# Load model state
+			self.model.load_state_dict(ckpt['model_state'])
+			# Load optimizer state
+			self.optimizer.load_state_dict(ckpt['optimizer_state'])
+			# Load scheduler state
+			self.scheduler.load_state_dict(ckpt['scheduler_state'])
+			# Load history
+			self.history = ckpt['history']
+			self.total_iters = ckpt['total_iters']
+			self.cur_epoch = ckpt['cur_epoch']
+
+		else:
+			print('No checkpoint found at: {}'.format(ckpt))
+
+
+
